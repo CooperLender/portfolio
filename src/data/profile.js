b76355f4f805afc8fdfb7d3e843c0f4f932bae5e
Cooper Lender <cooperlender@Coopers-MacBook-Air-3.local>
2026-01-01 15:01:40 -0700

    Unify EN/JA layout and add shared contact links

---
+++ src/data/profile.js
@@ -0,0 +1,7 @@
+export const profile = {
+    name: "Cooper Lender",
+    email: "YOUR_EMAIL_HERE",
+    github: "https://github.com/CooperLender",   // change if you prefer TheQuartzLord
+    linkedin: "https://www.linkedin.com/in/YOUR_LINKEDIN",
+  };
+  
